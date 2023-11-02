d5182ecfb64f842a12436067dbbdce67739a2c71
wonhyoung0118 <minwh0118@gmail.com>
2023-11-02 13:10:20 +0900

    test1

--- src/main/java/kr/ac/mjc/blog/repository/BlogRepository.java
+++ src/main/java/kr/ac/mjc/blog/repository/BlogRepository.java
@@ -4,4 +4,5 @@ import kr.ac.mjc.blog.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BlogRepository extends JpaRepository<Article, Long> {
+    
 }
